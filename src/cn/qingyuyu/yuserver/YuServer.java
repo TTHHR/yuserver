289d4b2ec0e087e6a8087786fe25daa93f8b7e68
TTHHR <harrytit@foxmail.com>
2017-05-02 23:26:57 +0800

    add Log Tool

--- src/cn/qingyuyu/yuserver/YuServer.java
+++ src/cn/qingyuyu/yuserver/YuServer.java
@@ -4,29 +4,41 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.*;
 
+import cn.qingyuyu.yuserver.util.Log;
 
 public class YuServer {
-public static void main(String [] str)
-{
-	final int THREAD_MAX=150;//max thread number,you can change it<255
-	final int PORT=2333;//listen port
-	Thread listener = new Thread(new Runnable() {// new listen thread
-		public void run() {
-			try {
-				@SuppressWarnings("resource")
-				ServerSocket svrSocket = new ServerSocket(PORT); // listen socket
-				ExecutorService fixedThreadPool = Executors.newFixedThreadPool(THREAD_MAX);  
-				while (true) {
-					Socket socket = svrSocket.accept(); // wait for request
-					fixedThreadPool.execute(new YuThread(socket));
-					// 开启子线程处理请求
-				}
-			} catch (Exception e) {
-				System.out.println(e.toString());
+	public static void main(String[] str) {
+		final int THREAD_MAX = 150;// max thread number,you can change it<255
+		final int PORT = 2333;// listen port
+		try {
+			@SuppressWarnings("resource")
+			ServerSocket svrSocket = new ServerSocket(PORT); // listen socket
+			ExecutorService fixedThreadPool = Executors.newFixedThreadPool(THREAD_MAX); // Thread
+																						// pool
+			while (true) {
+				Socket socket = svrSocket.accept(); // wait for request
+				Thread manager = new Thread(new Runnable() {// new manager
+															// thread
+					FutureTask<Boolean> futureTask = new FutureTask<Boolean>(new YuThread(socket));
+					public void run() {
+						try {
+							
+							fixedThreadPool.execute(futureTask);
+							futureTask.get(5000, TimeUnit.MILLISECONDS);
+						} catch (Exception e) {
+							futureTask.cancel(true);//cancel this timeout thread
+							Log.getInstance().e("threadManager", e.toString());
+						}
+					}
+				});
+				manager.start();
+				// luncher thread to deal the request
 			}
 		}
-	});
 
-	listener.start();// 开启监听线程
-}
+		catch (Exception e) {
+			Log.getInstance().e("listenPort", e.toString());
+		}
+
+	}
 }
--- src/cn/qingyuyu/yuserver/YuThread.java
+++ src/cn/qingyuyu/yuserver/YuThread.java
@@ -1,19 +1,30 @@
 package cn.qingyuyu.yuserver;
-import java.net.*;
 
-public class YuThread implements Runnable
-{
+import java.net.*;
+import java.util.Random;
+import java.util.concurrent.Callable;
+
+import cn.qingyuyu.yuserver.util.Log;
+
+public class YuThread implements Callable<Boolean> {
 	Socket s;
-YuThread(Socket s)
-{
-	this.s=s;
-}
-	@Override
-	public void run()
-	{
-		// TODO: Implement this method
-		System.out.println("son thread"+s.getRemoteSocketAddress());
+
+	YuThread(Socket s) {
+		this.s = s;
 	}
-	
-	
+
+	@Override
+	public Boolean call() throws Exception {
+		// TODO Auto-generated method stub
+		try {
+			Random random = new Random();
+			int rum = random.nextInt(10);
+			Thread.sleep(rum * 1000);
+			Log.getInstance().d("socket", s.toString());
+		} catch (Exception e) {
+			Log.getInstance().e("runTime", e.toString());
+			return false;
+		}
+		return true;
+	}
 }
