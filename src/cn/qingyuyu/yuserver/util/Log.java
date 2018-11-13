b64d119ac0c06e9452c9bcfbac81368c17fd6d8f
tthhr <harrytit@foxmail.com>
2018-11-13 11:14:08 +0800

    修复Log

--- src/cn/qingyuyu/yuserver/util/Log.java
+++ src/cn/qingyuyu/yuserver/util/Log.java
@@ -10,23 +10,47 @@ import java.util.Date;
 public class Log {
 	 static Log l = null;
 	static BufferedWriter bw = null;
+	private String fileName="";
 
 	private Log() {
+		if(l!=null)
+		{
+			l.closeFile();
+		}
 		try {
 			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");// set date
 																		// format
-			File logFile = new File(df.format(new Date()).toString() + ".log");
+			File logFile = new File(df.format(new Date()) + ".log");
 			if (!logFile.exists())
 				logFile.createNewFile();
+
+			fileName=logFile.getName();
 			bw = new BufferedWriter(new FileWriter(df.format(new Date()).toString() + ".log", true));
 		} catch (Exception e) {
 			System.out.println("Log File Error");
 		}
 	}
 
+	private void closeFile()
+	{
+		if(bw!=null) {
+			try {
+				bw.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 	public static Log getInstance() {
+		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");// set date
 		if(l==null)
 			l=new Log();
+		else if(l.fileName.equals(df.format(new Date()) + ".log"))
+		{
+			l=new Log();
+		}
+
 		return l;
 	}
 
